564c7d65195f7eb9294a8684fd7a6d8c5f5147ab
Pedro Oliveira <poliveira199208@gmail.com>
2019-04-27 15:19:44 +0100

    Json Response HTTP request

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-import os
+import pandas as pd
+import numpy as np
 import subprocess
-import LSTM_model
+import json
+
 
 app = Flask(__name__)
 
@@ -21,7 +23,19 @@ def process():
     print(option2)
     print(option3)
     subprocess.call('python LSTM_model.py ' + option3 + ' ' + option1 + ' ' + option2)
-    return jsonify({'tipo': option3})
+    data = pd.read_csv("predictiontestPantspreco.csv")
+    print(data)
+    data.drop(["Atual"],axis=1, inplace=True)
+    print(data)
+    data_matrix = data.to_records()
+    print(data_matrix)
+
+    js = {}
+    for i in data_matrix.tolist():
+        js[i[0]] = i[1]
+
+    print(json.dumps(js))
+    return jsonify(js)
 
 if __name__ == '__main__':
     app.run()
